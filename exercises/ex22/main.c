0dfe591d655657cc40c3033420d73c19c4651cdc
Luna <luna.h199518@gmail.com>
2026-08-24 08:57:41 +0000

    refactor: add note of TA's src code

--- exercises/ex22/main.c
+++ exercises/ex22/main.c
@@ -31,6 +31,8 @@ static int hextoi(const char* s, int n) {
     int r = 0;
     while (n-- > 0) {
         r = r << 4;
+        // 每個十六進位字元只佔 4 bits，因此每讀入一個新的 hex digit時，
+        // 要先將目前結果左移 4 bits，才能正確加入該數值。
         if (*s >= 'A')
             r += *s++ - 'A' + 10;
         else if (*s >= 0)
@@ -60,27 +62,27 @@ void initrd_cat(const void* rd, const char* filename) {
 
 int main() {
     /* Prepare the initial RAM disk */
-    FILE* fp = fopen("initramfs.cpio", "rb");
+    FILE* fp = fopen("initramfs.cpio", "rb"); // 以二進位讀檔
     if (!fp) {
-        perror("fopen");
+        perror("fopen"); // 如果無法開檔，以 fopen: sys message 印出錯誤訊息
         return EXIT_FAILURE;
     }
-    fseek(fp, 0, SEEK_END);
-    long sz = ftell(fp);
-    void* rd = malloc(sz);
-    fseek(fp, 0, SEEK_SET);
-    if (fread(rd, 1, sz, fp) != sz) {
-        fprintf(stderr, "Failed to read the device tree blob\n");
+    fseek(fp, 0, SEEK_END); // 將指標移動到檔案結尾
+    long sz = ftell(fp); // 計算指標距離檔案開頭的位元組數 aka 取得檔案大小
+    void* rd = malloc(sz); // 分配記憶體
+    fseek(fp, 0, SEEK_SET); // 將指標移動到檔案開頭
+    if (fread(rd, 1, sz, fp) != sz) { // 如果存入 buffer 的大小不等於檔案大小
+        fprintf(stderr, "Failed to read the initial RAM disk\n");
         free(rd);
         fclose(fp);
         return EXIT_FAILURE;
     }
     fclose(fp);
 
-    initrd_list(rd);
-    initrd_cat(rd, "osc.txt");
-    initrd_cat(rd, "test.txt");
+    initrd_list(rd); //列出 buffer 中所有來自 initramfs.cpio 的檔案名稱和大小
+    initrd_cat(rd, "osc.txt"); // 尋找並輸出 osc.txt 的內容；若不存在則回報
+    initrd_cat(rd, "test.txt"); 
 
-    free(rd);
+    free(rd); // free buffer，釋放記憶體
     return 0;
 }
